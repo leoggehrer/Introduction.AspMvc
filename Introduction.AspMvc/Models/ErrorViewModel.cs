8fc440056a93c67d3b07c6adb3f7a547033560d2
Gehrer Gerhard <g.gehrer@htl-leonding.ac.at>
2022-03-28 10:59:52 +0200

    Add project files.

---
+++ Introduction.AspMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Introduction.AspMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Introduction.AspMvc/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace Introduction.AspMvc.Models
+{
+    public class Student
+    {
+        private static int Counter = 0;
+        public Student()
+        {
+            Id = ++Counter;
+        }
+        public int Id { get; set; }
+        public string MatriculationNumber { get; set; } = string.Empty;
+        public string Firstname { get; set; } = string.Empty;
+        public string Lastname { get; set; } = string.Empty;
+    }
+}
